709f2a3968715f4ec9477bd7dd4817e713df6998
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-06-14 16:20:17 -0300

    test

--- pages/pages.js
+++ pages/pages.js
@@ -2,8 +2,6 @@ $(function () {
     $(document).ready(function () {
         $('.sidenav').sidenav();
     });
-    console.log('Oi');
-
     let aux = $('.dropdown-trigger');
     aux.dropdown();
 });
@@ -13,7 +11,6 @@ $(function () {
     let menu = isLogeedIn ? '../menu.html' : '../menu-anonimo.html';
     $('#menu').load(menu);
     $('#footer').load('../footer.html');
-    console.log('Oi');
 
     window.setTimeout(function () {
         var elems = document.querySelectorAll('.dropdown-trigger');
