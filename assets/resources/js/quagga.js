959cf62d14f5c0c55ae16cfccaf30103fb21e0bc
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-06-20 04:16:51 -0300

    feature: creation of the barcode reader resource

--- assets/resources/js/quagga.js
+++ assets/resources/js/quagga.js
@@ -1,26 +1,48 @@
-// Quagga start
-Quagga.init(
-    {
-        inputStream: {
-            name: 'Live',
-            type: 'LiveStream',
-            target: document.querySelector('#camera'), // Or '#yourElement' (optional)
+function iniciarLeitor() {
+    // iniciar camera
+    Quagga.init(
+        {
+            inputStream: {
+                name: 'Live',
+                type: 'LiveStream',
+                target: document.querySelector('#camera'), // Or '#yourElement' (optional)
+            },
+            decoder: {
+                readers: ['ean_reader'],
+            },
+            multiple: false,
         },
-        decoder: {
-            readers: ['code_128_reader'],
+        function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Initialization finished. Ready to start');
+            Quagga.start();
         },
-    },
-    function (err) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('Initialization finished. Ready to start');
-        Quagga.start();
-    },
-);
+    );
 
-Quagga.onDetected((data) => {
-    console.log(data);
-    document.querySelector('#resultado').innerText = data.codeResult;
-});
+    // logica do codigo de barras
+    let token = 'FZuLNEaq-XQGuNKG3i7sMg';
+    Quagga.onDetected((data) => {
+        //let test = data.codeResult.code;
+        let test = 7891150064331;
+        Quagga.stop();
+        postData();
+        async function postData(
+            url = `https://api.cosmos.bluesoft.com.br/gtins/${test}.json`,
+            data = {},
+        ) {
+            // Default options are marked with *
+            const req = await fetch(url, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Cosmos-Token': token,
+                    'User-Agent': 'Cosmos-API-Request',
+                },
+            });
+            let aux = await req.json();
+            console.log(aux);
+        }
+    });
+}
